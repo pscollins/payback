e0b0a646376d33cd8eeb0f6cff23ada48ddf3cb2
Falcon Dai <me@falcondai.com>
2014-06-29 06:55:39 -0500

    server.py -> views.py

--- service/__init__.py
+++ service/__init__.py
@@ -3,10 +3,13 @@
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
 
-DB_PATH = ""
-
 app = Flask(__name__, static_url_path='')
-app.config.from_pyfile('./local-dev.cfg')
+
+# debug with local mongoDB
+#app.config.from_pyfile('./local-dev.cfg')
+
+# debug with remtoe mongoDB
+app.config.from_pyfile('./remote-dev.cfg')
 
 # def init_db():
 db = MongoEngine(app)
--- service/views.py
+++ service/views.py
@@ -114,4 +114,4 @@ def profile():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
