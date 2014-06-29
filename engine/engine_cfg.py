baeb446b85d25e5ccabdb6c06fb4f3f8eb226761
Patrick <pscollins@uchicago.edu>
2014-06-29 10:00:00 -0500

    hello

--- engine/engine_cfg.py
+++ engine/engine_cfg.py
@@ -1,7 +1,7 @@
 # Testing credentials! Don't worry.
 
 TW_CLIENT_ID = "ACe2cfa94dbe756f51d5db29dc32f2fe7c"
-TW_SECRET_KEY = "2644baf7f1ea16f5d5fe83d4cacbfa1a"
+TW_SECRET_KEY = "a91a7e67cc6d8242af61e15d8102246b"
 
 VM_CLIENT_ID = "1795"
 VM_SECRET_KEY = "sjCfbnLEWMVMbFNs93rQ2KD5uucrexvu"
