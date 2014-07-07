5a9b3eb2886cef1da9433c69dfc8c73c34a9af8e
pscollins@uchicago.edu <pscollins@uchicago.edu>
2014-07-07 08:55:23 -0500

    dropping uneeded dependency

--- do_setup.sh
+++ do_setup.sh
@@ -4,5 +4,4 @@ make make-env
 source activate
 pip install -r requirements.txt
 ./fix-path.sh
-pip install git+https://github.com/pythonforfacebook/facebook-sdk.git
 pip install git+https://github.com/geraldbaeck/Face-Recognition-Training-for-Sky-Biometry-API.git
