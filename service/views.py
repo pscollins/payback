731235b5062082faf3b616653f0e2b4cb84d99e5
pscollins@uchicago.edu <pscollins@uchicago.edu>
2014-07-13 02:34:02 -0500

    missing colon

--- service/views.py
+++ service/views.py
@@ -249,8 +249,8 @@ def apply_uploaded_file():
     return redirect(url_for('profile'))
 
 # endpoint for mobile to hit
-@app.route("/venmo_auth_url", methods=["GET"])
-return {"url": venmo.get_auth_url()}
+@app.route("/venmo_auth_url", methods=["GET"]):
+    return {"url": venmo.get_auth_url()}
 
 @app.route("/", methods=["GET"])
 def render_login():
